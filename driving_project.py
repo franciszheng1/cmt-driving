d7b736863888ac46de43879674a779d3c4e9ab0f
Francis Zheng <franciszheng1@outlook.com>
2025-11-01 21:58:04 -0400

    Added imports, plotting setup and password login for the program

--- driving_project.py
+++ driving_project.py
@@ -1,5 +1,24 @@
-import matplotlib
-matplotlib.use("MacOSX")  # ensure plot windows on macOS
+import sys
+import pandas as pd     # for handling tabular data
+import numpy as np      # for random number generation
+import hashlib          # for cybersecurity (hashing)
+import matplotlib       # for graphs and plots
 
-if __name__ == "__main__":
-    print("Project scaffold ready. Next step: add simulator + plots.")
+# Choose a Matplotlib backend that works for all systems
+# "MacOSX" -> macOS pop-up, "TkAgg" -> Windows/Linux
+matplotlib.use("MacOSX" if sys.platform == "darwin" else "TkAgg")
+
+import matplotlib.pyplot as plt # can now plot graphs
+
+# Asks for a password before running the program
+password = input("Enter password to access driving data: ")
+
+# Checks if the entered password is correct
+if password != "secure123":
+    print("Access denied! Exiting program...")
+    exit() # Stops the program immediately
+else:
+    print("Access granted!\n")
+
+
+
